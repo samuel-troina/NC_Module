51c7630e4197c246adfcd0fa2a6153b09ea90fda
Samuel Santos Troina <samuel.troina@gmail.com>
2024-09-04 14:12:40 -0300

    Inclusão das funções sendCmd, configureLoRaWAN e configureOTAA

--- NC_module.cpp
+++ NC_module.cpp
@@ -4,7 +4,7 @@ void NC_module::init(Stream &serial) {
     _serial = &serial;
 }
 
-bool NC_module::sendCmd(String cmd, int timeout=5000){
+bool NC_module::sendCmd(String cmd, int timeout){
   _serial.println(cmd);
 
   String responseBuffer;
@@ -20,4 +20,26 @@ bool NC_module::sendCmd(String cmd, int timeout=5000){
 
 bool NC_module::moduleStatus(){
     return sendCmd("AT+STATUS?");
+}
+
+bool NC_module::configureLoRaWAN(String SUBBAND, String CH, String DR, String DRX2, String FQRX2, String DLRX2){
+    if (!sendCmd("AT+SUBBAND="+SUBBAND)) return false;
+    if (!sendCmd("AT+CHANNEL="+CH)) return false;
+    if (!sendCmd("AT+DATARATE="+DR)) return false;
+
+    sendCmd("AT+TXPOWER=20");
+
+    if (!sendCmd("AT+DATARATERX2="+DRX2)) return false;
+    if (!sendCmd("AT+FQRX2="+FQRX2)) return false;
+    if (!sendCmd("AT+RX2DELAY="+DLRX2)) return false;
+
+    return true;
+}
+
+bool NC_module::configureOTAA(String DEVEUI, String APPEUI, String APPKey){
+    if (!sendCmd("AT+DEVEUI="+DEVEUI)) return false;
+    if (!sendCmd("AT+APPEUI="+APPEUI)) return false;
+    if (!sendCmd("AT+APPKey="+APPKey)) return false;
+
+    return true;
 }
--- NC_module.h
+++ NC_module.h
@@ -8,7 +8,10 @@ class NC_module {
         Stream* _serial;
     public:
         void init(Stream &serial);
+        bool sendCmd(String cmd, int timeout=5000);
         bool moduleStatus();
+        bool configureLoRaWAN(String SUBBAND, String CH, String DR, String DRX2, String FQRX2, String DLRX2);
+        bool configureOTAA(String DEVEUI, String APPEUI, String APPKey);
 };
 
 #endif
